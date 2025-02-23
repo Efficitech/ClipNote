d5a9330e98ba10ee168ad9632d67bb7300eb42e1
GITGUtter123 <141160037+GITGUtter123@users.noreply.github.com>
2025-02-22 21:35:39 -0500

    Delete content.js

--- content.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("mouseup", function () {
-    let selectedText = window.getSelection().toString().trim();
-    if (selectedText.length > 0) {
-        chrome.storage.local.set({ "highlightedText": selectedText });
-    }
-});
